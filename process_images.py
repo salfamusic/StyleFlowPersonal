32e9427ce8e0fd9e22038314eb0e727791ba96a8
Michael King <salfa.music@gmail.com>
2021-02-25 17:59:16 -0600

    all together

--- process_images.py
+++ process_images.py
@@ -2,15 +2,23 @@ import DPRAPI from dprapi
 import S2EAPI from s2eapi
 import AzureAPI from azure
 
-KEY = "8ba735f8bc8743cdb3299577b050e555"
 
-# This endpoint will be used in all examples in this quickstart.
-ENDPOINT = "https://facerec1234.cognitiveservices.azure.com/"
+def process_images(
+    azure_key, 
+    azure_endpoint,
+    raw_dir = './work_dir/raw',
+    aligned_dir = './work_dir/aligned',
+    projected_dir = './work_dir/projected',
+    lights_dir = './work_dir/lights',
+    azure_dir = './work_dir/azure',
+    netowrk_pkl = 'https://drive.google.com/uc?id=1IxRyfTf62KBjyc486JA5tGLVnFh_d4eO',
+    vgg_pkl = 'https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2'):
+    s2e_api = S2EAPI(raw_dir, aligned_dir, projected_dir, netowrk_pkl, vgg_pkl)
+    dpr_api = DPRAPI(aligned_dir, lights_dir)
+    azure_api = AzureAPI(aligned_dir, azure_dir, azure_key, azure_endpoint)
 
-def process_images(azure_key, azure_endpoint):
-    dpr_api = DPRAPI('./work_dir/aligned', './work_dir/lights')
-    s2e_api = S2EAPI('./work_dir/raw', './work_dir/aligned', './work_dir/projected', 'https://drive.google.com/uc?id=1IxRyfTf62KBjyc486JA5tGLVnFh_d4eO', 'https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2')
-    azure_apid = AzureAPI('./work_dir/aligned', './work_dir/azure', azure_key, azure_endpoint)
-
-if __name__ == '__main__':
+    s2e_api.align()
+    s2e_api.project()
+    dpr_api.predict_light()
+    azure_api.infer_face_features()
     
