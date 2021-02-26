3292df6490c5e4c65594f6bf4248a48a36a365a5
Michael King <salfa.music@gmail.com>
2021-02-26 16:22:31 -0600

    allowing skips

--- process_images.py
+++ process_images.py
@@ -14,13 +14,22 @@ def process_images(
     aligned_dir = './work_dir/aligned',
     results_dir = './work_dir/results',
     network_pkl_gcloud_id = '1xAL82ELuRciaYR5PDQYaH2MHy6-UbkY-',
-    vgg_pkl_gcloud_id = '1ofUti9VTZf2zqqDocuXqGrrZHI2JKh8p'):
+    vgg_pkl_gcloud_id = '1ofUti9VTZf2zqqDocuXqGrrZHI2JKh8p',
+    skip_projection = False,
+    skip_dpr = False,
+    skip_azure = False):
     s2e_api = S2EAPI(raw_dir, aligned_dir, results_dir, network_pkl_gcloud_id, vgg_pkl_gcloud_id)
     dpr_api = DPRAPI(aligned_dir, results_dir)
     azure_api = AzureAPI(aligned_dir, results_dir, azure_key, azure_endpoint)
 
-    s2e_api.align()
-    s2e_api.project()
-    dpr_api.predict_light()
-    azure_api.infer_face_features()
+
+    if not skip_projection:
+        s2e_api.align()
+        s2e_api.project()
+
+    if not skip_dpr:
+        dpr_api.predict_light()
+
+    if not skip_azure:
+        azure_api.infer_face_features()
     
